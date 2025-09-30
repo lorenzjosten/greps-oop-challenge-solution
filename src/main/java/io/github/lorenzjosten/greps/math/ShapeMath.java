6eaffd7d840d03864372d3bcf87707fb360e22c9
Lorenz Josten <lorenz.josten@posteo.net>
2025-09-30 10:50:18 +0200

    feat: implement ShapeMath

--- src/main/java/io/github/lorenzjosten/greps/math/ShapeMath.java
+++ src/main/java/io/github/lorenzjosten/greps/math/ShapeMath.java
@@ -1,20 +1,29 @@
 package io.github.lorenzjosten.greps.math;
 
+import io.github.lorenzjosten.greps.model.builder.IShapeBuilder;
+import io.github.lorenzjosten.greps.model.builder.ShapeBuilderImpl;
+import io.github.lorenzjosten.greps.model.value.AShape;
 import io.github.lorenzjosten.greps.model.value.Shape;
 
 /**
  * Given by the challenge. Signatures should not be changed.
  */
 public final class ShapeMath {
+    private static final IShapeBuilder builder = new ShapeBuilderImpl();
+
     private ShapeMath() {
 
     }
 
     public static double perimeter(Shape shape, double... parameters) {
-        return 0;
+        return create(shape, parameters).perimeter();
     }
 
     public static double area(Shape shape, double... parameters) {
-        return 0;
+        return create(shape, parameters).area();
+    }
+
+    private static AShape create(Shape shape, double... parameters) {
+        return builder.type(shape).parameters(parameters).build();
     }
 }
