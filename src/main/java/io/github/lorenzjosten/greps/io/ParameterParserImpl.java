33bb070132e6c530a73542b33ab0b983512746e5
Lorenz Josten <lorenz.josten@posteo.net>
2025-09-30 13:01:41 +0200

    feat: refactor ParameterParserImpl

--- src/main/java/io/github/lorenzjosten/greps/io/ParameterParserImpl.java
+++ src/main/java/io/github/lorenzjosten/greps/io/ParameterParserImpl.java
@@ -1,54 +1,88 @@
 package io.github.lorenzjosten.greps.io;
 
+import io.github.lorenzjosten.greps.model.value.CircleParameters;
+import io.github.lorenzjosten.greps.model.value.RectangleParameters;
+import io.github.lorenzjosten.greps.model.value.SquareParameters;
+
 public class ParameterParserImpl implements IParameterParser {
     private static final int PARAMETER_COUNT_SQUARE = 1;
-    private static final int PARAMETER_COUNT_RECTANGLE = 2;
-    private static final int PARAMETER_COUNT_CIRCLE = 1;
-
     private static final int PARAMETER_INDEX_SQUARE_LENGTH = 0;
+
+    private static final int PARAMETER_COUNT_RECTANGLE = 2;
     private static final int PARAMETER_INDEX_RECTANGLE_LENGTH = 0;
     private static final int PARAMETER_INDEX_RECTANGLE_WIDTH = 1;
+
+    private static final int PARAMETER_COUNT_CIRCLE = 1;
     private static final int PARAMETER_INDEX_CIRCLE_RADIUS = 0;
 
-    private final double[] parameters;
+    private SquareParameters squareParameters = null;
+    private RectangleParameters rectangleParameters = null;
+    private CircleParameters circleParameters = null;
 
-    public ParameterParserImpl(double... parameters) {
-        this.parameters = parameters;
+    @Override
+    public SquareParameters squareParameters() {
+        if (squareParameters == null) {
+            throw new IllegalArgumentException("Missing parameter length for square.");
+        }
+
+        return squareParameters;
     }
 
     @Override
-    public double getSquareLength() {
-        if (parameters.length < PARAMETER_COUNT_SQUARE) {
-            throw new IllegalArgumentException("Missing parameter square length.");
+    public RectangleParameters rectangleParameters() {
+        if (rectangleParameters == null) {
+            throw new IllegalArgumentException("Missing parameters length and width for rectangle.");
         }
 
-        return parameters[PARAMETER_INDEX_SQUARE_LENGTH];
+        return rectangleParameters;
     }
 
     @Override
-    public double getRectangleLength() {
-        if (parameters.length < PARAMETER_COUNT_RECTANGLE) {
-            throw new IllegalArgumentException("Missing parameter rectangle width / length.");
+    public CircleParameters circleParameters() {
+        if (circleParameters == null) {
+            throw new IllegalArgumentException("Missing parameter radius for circle.");
         }
 
-        return parameters[PARAMETER_INDEX_RECTANGLE_LENGTH];
+        return circleParameters;
     }
 
     @Override
-    public double getRectangleWidth() {
-        if (parameters.length < PARAMETER_COUNT_RECTANGLE) {
-            throw new IllegalArgumentException("Missing parameter rectangle width / length.");
-        }
+    public void parse(double... parameters) {
+        parseSquareParameters(parameters);
 
-        return parameters[PARAMETER_INDEX_RECTANGLE_WIDTH];
+        parseRectangleParameters(parameters);
+
+        parseCircleParameters(parameters);
     }
 
-    @Override
-    public double getCircleRadius() {
-        if (parameters.length < PARAMETER_COUNT_CIRCLE) {
-            throw new IllegalArgumentException("Missing parameter circle radius.");
-        }
+    private void parseCircleParameters(double[] parameters) {
+        if (parameters.length >= PARAMETER_COUNT_CIRCLE) {
+            double radius = parameters[PARAMETER_INDEX_CIRCLE_RADIUS];
 
-        return parameters[PARAMETER_INDEX_CIRCLE_RADIUS];
+            circleParameters = new CircleParameters(radius);
+        } else {
+            circleParameters = null;
+        }
+    }
+
+    private void parseRectangleParameters(double[] parameters) {
+        if (parameters.length >= PARAMETER_COUNT_RECTANGLE) {
+            double length = parameters[PARAMETER_INDEX_RECTANGLE_LENGTH];
+            double width = parameters[PARAMETER_INDEX_RECTANGLE_WIDTH];
+
+            rectangleParameters = new RectangleParameters(length, width);
+        } else {
+            circleParameters = null;
+        }
+    }
+
+    private void parseSquareParameters(double... parameters) {
+        if (parameters.length >= PARAMETER_COUNT_SQUARE) {
+            double length = parameters[PARAMETER_INDEX_SQUARE_LENGTH];
+
+            squareParameters = new SquareParameters(length);
+        } else {
+            squareParameters = null;
+        }
     }
 }
