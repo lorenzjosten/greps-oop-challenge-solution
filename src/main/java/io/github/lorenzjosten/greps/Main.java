e92d927acb4cf4072a1e8c0e370aa466b4a89a64
Lorenz Josten <lorenz.josten@posteo.net>
2025-09-30 09:46:07 +0200

    feat: add demo program to verify working solution

--- src/main/java/io/github/lorenzjosten/greps/Main.java
+++ src/main/java/io/github/lorenzjosten/greps/Main.java
@@ -1,7 +1,26 @@
 package io.github.lorenzjosten.greps;
 
+import io.github.lorenzjosten.greps.math.ShapeMath;
+import io.github.lorenzjosten.greps.model.value.Shape;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from main.");
+        var squarePerimeter = ShapeMath.perimeter(Shape.SQUARE, 2);
+        System.out.println("Perimeter of 2x2 square: " + squarePerimeter);
+
+        var squareArea = ShapeMath.perimeter(Shape.SQUARE, 2);
+        System.out.println("Area of 2x2 square: " + squareArea);
+
+        var rectanglePerimeter = ShapeMath.perimeter(Shape.RECTANGLE, 2, 3);
+        System.out.println("Perimeter of 2x3 rectangle: " + rectanglePerimeter);
+
+        var rectangleArea = ShapeMath.perimeter(Shape.RECTANGLE, 2, 3);
+        System.out.println("Area of 2x3 rectangle: " + rectangleArea);
+
+        var circlePerimeter = ShapeMath.perimeter(Shape.CIRCLE, 2);
+        System.out.println("Perimeter of circle with radius 2: " + circlePerimeter);
+
+        var circleArea = ShapeMath.perimeter(Shape.CIRCLE, 2);
+        System.out.println("Area of circle with radius 2: " + circleArea);
     }
 }
