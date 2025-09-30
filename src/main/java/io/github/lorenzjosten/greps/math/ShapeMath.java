6cfa88b0f16cbc82692c2ba410d2cf9978eb3555
Lorenz Josten <lorenz.josten@posteo.net>
2025-09-30 15:35:43 +0200

    feat: use decoupled parser and builder in ShapeMath

--- src/main/java/io/github/lorenzjosten/greps/math/ShapeMath.java
+++ src/main/java/io/github/lorenzjosten/greps/math/ShapeMath.java
@@ -1,29 +1,33 @@
 package io.github.lorenzjosten.greps.math;
 
+import io.github.lorenzjosten.greps.io.IParameterParser;
+import io.github.lorenzjosten.greps.io.ParameterParserImpl;
 import io.github.lorenzjosten.greps.model.builder.IShapeBuilder;
 import io.github.lorenzjosten.greps.model.builder.ShapeBuilderImpl;
-import io.github.lorenzjosten.greps.model.value.AShape;
+import io.github.lorenzjosten.greps.model.value.IShape;
+import io.github.lorenzjosten.greps.model.value.IShapeParameters;
 import io.github.lorenzjosten.greps.model.value.Shape;
 
 /**
  * Given by the challenge. Signatures should not be changed.
  */
 public final class ShapeMath {
+    private static final IParameterParser parser = new ParameterParserImpl();
     private static final IShapeBuilder builder = new ShapeBuilderImpl();
 
-    private ShapeMath() {
-
-    }
+    private ShapeMath() {}
 
     public static double perimeter(Shape shape, double... parameters) {
-        return create(shape, parameters).perimeter();
+        return createShape(shape, parameters).perimeter();
     }
 
     public static double area(Shape shape, double... parameters) {
-        return create(shape, parameters).area();
+        return createShape(shape, parameters).area();
     }
 
-    private static AShape create(Shape shape, double... parameters) {
-        return builder.type(shape).parameters(parameters).build();
+    private static IShape createShape(Shape shape, double... parameters) {
+        IShapeParameters shapeParameters = parser.parse(shape, parameters);
+
+        return builder.type(shape).parameters(shapeParameters).build();
     }
 }
