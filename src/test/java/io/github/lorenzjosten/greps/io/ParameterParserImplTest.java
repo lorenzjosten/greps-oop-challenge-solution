09dd2fd202a864e7954c7339fdf3ddffc0421439
Lorenz Josten <lorenz.josten@posteo.net>
2025-09-30 13:02:04 +0200

    test: refactor unit tests for ParameterParserImpl

--- src/test/java/io/github/lorenzjosten/greps/io/ParameterParserImplTest.java
+++ src/test/java/io/github/lorenzjosten/greps/io/ParameterParserImplTest.java
@@ -1,64 +1,74 @@
 package io.github.lorenzjosten.greps.io;
 
+import io.github.lorenzjosten.greps.model.value.CircleParameters;
+import io.github.lorenzjosten.greps.model.value.RectangleParameters;
+import io.github.lorenzjosten.greps.model.value.SquareParameters;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParameterParserImplTest {
-    @Test
-    public void should_parse_square_length() {
-        IParameterParser parser = new ParameterParserImpl(1);
 
-        assertEquals(1, parser.getSquareLength());
+    private final double[] NO_PARAMETER = { };
+    private final double[] ONE_PARAMETER = { 1 };
+    private final double[] TWO_PARAMETERS = { 1, 2 };
+
+    private final IParameterParser parser = new ParameterParserImpl();
+
+    @Test
+    public void should_parse_no_parameter() {
+        assertDoesNotThrow(() -> parser.parse(NO_PARAMETER));
     }
 
     @Test
-    public void should_parse_rectangle_length() {
-        IParameterParser parser = new ParameterParserImpl(1, 2);
-
-        assertEquals(1, parser.getRectangleLength());
+    public void should_parse_one_parameter() {
+        assertDoesNotThrow(() -> parser.parse(ONE_PARAMETER));
     }
 
     @Test
-    public void should_parse_rectangle_width() {
-        IParameterParser parser = new ParameterParserImpl(1, 2);
-
-        assertEquals(2, parser.getRectangleWidth());
+    public void should_parse_two_parameters() {
+        assertDoesNotThrow(() -> parser.parse(TWO_PARAMETERS));
     }
 
     @Test
-    public void should_parse_circle_radius() {
-        IParameterParser parser = new ParameterParserImpl(1);
+    public void should_retrieve_square_parameters() {
+        parser.parse(ONE_PARAMETER);
 
-        assertEquals(1, parser.getCircleRadius());
+        assertEquals(new SquareParameters(1), parser.squareParameters());
     }
 
     @Test
-    public void should_throw_when_no_square_length() {
-        IParameterParser parser = new ParameterParserImpl();
+    public void should_retrieve_rectangle_parameters() {
+        assertDoesNotThrow(() -> parser.parse(TWO_PARAMETERS));
 
-        assertThrows(IllegalArgumentException.class, parser::getSquareLength);
+        assertEquals(new RectangleParameters(1, 2), parser.rectangleParameters());
     }
 
     @Test
-    public void should_throw_when_no_rectangle_length() {
-        IParameterParser parser = new ParameterParserImpl();
+    public void should_retrieve_circle_parameters() {
+        parser.parse(ONE_PARAMETER);
 
-        assertThrows(IllegalArgumentException.class, parser::getRectangleLength);
+        assertEquals(new CircleParameters(1), parser.circleParameters());
     }
 
     @Test
-    public void should_throw_when_no_rectangle_width() {
-        IParameterParser parser = new ParameterParserImpl(1);
+    public void should_throw_when_null_square_parameters() {
+        parser.parse(NO_PARAMETER);
 
-        assertThrows(IllegalArgumentException.class, parser::getRectangleWidth);
+        assertThrows(IllegalArgumentException.class, parser::squareParameters);
     }
 
     @Test
-    public void should_throw_when_no_circle_radius() {
-        IParameterParser parser = new ParameterParserImpl();
+    public void should_throw_when_null_rectangle_parameters() {
+        parser.parse(ONE_PARAMETER);
 
-        assertThrows(IllegalArgumentException.class, parser::getCircleRadius);
+        assertThrows(IllegalArgumentException.class, parser::rectangleParameters);
+    }
+
+    @Test
+    public void should_throw_when_null_circle_parameters() {
+        parser.parse(NO_PARAMETER);
+
+        assertThrows(IllegalArgumentException.class, parser::circleParameters);
     }
 }
