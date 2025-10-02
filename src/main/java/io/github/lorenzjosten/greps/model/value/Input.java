cb9c59fab3a0d66ea524501316d76ff33aaa2d69
Lorenz Josten <lorenz.josten@posteo.net>
2025-10-02 15:14:19 +0200

    feat: add convenience methods to Input

--- src/main/java/io/github/lorenzjosten/greps/model/value/Input.java
+++ src/main/java/io/github/lorenzjosten/greps/model/value/Input.java
@@ -2,4 +2,20 @@ package io.github.lorenzjosten.greps.model.value;
 
 public record Input(Shape type, double... parameters) {
 
+    public boolean isSquare() {
+        return type == Shape.SQUARE;
+    }
+
+    public boolean isRectangle() {
+        return type == Shape.RECTANGLE;
+    }
+
+    public boolean isCircle() {
+        return type == Shape.CIRCLE;
+    }
+
+    public boolean isShorterThan(int length) {
+        return parameters.length <= length;
+    }
+
 }
