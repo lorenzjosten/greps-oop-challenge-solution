693ae81091ea46c85e4bad68c6eecb9adcd05733
Lorenz Josten <lorenz.josten@posteo.net>
2025-10-02 15:53:10 +0200

    task: ShapeBuilder should not require shape type to build

--- src/main/java/io/github/lorenzjosten/greps/model/builder/IShapeBuilder.java
+++ src/main/java/io/github/lorenzjosten/greps/model/builder/IShapeBuilder.java
@@ -2,11 +2,8 @@ package io.github.lorenzjosten.greps.model.builder;
 
 import io.github.lorenzjosten.greps.model.value.IShape;
 import io.github.lorenzjosten.greps.model.value.IShapeParameters;
-import io.github.lorenzjosten.greps.model.value.Shape;
 
 public interface IShapeBuilder {
-
-    IShapeBuilder type(Shape shape);
 
     IShapeBuilder parameters(IShapeParameters parameters);
 
