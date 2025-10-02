c7ffedca806d7983eaf6bccac64fddaff898c9e7
Lorenz Josten <lorenz.josten@posteo.net>
2025-10-02 14:48:22 +0200

    feat: implement input validation

--- src/main/java/io/github/lorenzjosten/greps/io/validation/ValidatorImpl.java
+++ src/main/java/io/github/lorenzjosten/greps/io/validation/ValidatorImpl.java
@@ -2,9 +2,26 @@ package io.github.lorenzjosten.greps.io.validation;
 
 import io.github.lorenzjosten.greps.model.value.Shape;
 
+import static io.github.lorenzjosten.greps.model.value.Shape.*;
+
 public class ValidatorImpl implements IValidator {
+
+    private static final int PARAMETER_COUNT_CIRCLE = 1;
+    private static final int PARAMETER_COUNT_SQUARE = 1;
+    private static final int PARAMETER_COUNT_RECTANGLE = 2;
 
     @Override
     public void validate(Shape type, double... parameters) {
+        if (type == null) throw new IllegalArgumentException("Missing shape type.");
+
+        if (type == SQUARE && parameters.length < PARAMETER_COUNT_SQUARE)
+            throw new IllegalArgumentException("Missing parameters for square.");
+
+        if (type == RECTANGLE && parameters.length < PARAMETER_COUNT_RECTANGLE)
+            throw new IllegalArgumentException("Missing parameters for rectangle.");
+
+        if (type == CIRCLE && parameters.length < PARAMETER_COUNT_CIRCLE)
+            throw new IllegalArgumentException("Missing parameters for circle.");
     }
+
 }
